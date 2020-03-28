7a21a15c0e784ee6fc6b065cb2f8f430bb6fe3af
camsmy <camille.monis@gmail.com>
2020-03-29 00:53:44 +0800

    first commit

---
+++ all.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $surface = $('.surface');
+    $car = $('.car');
+
+//keypress event
+$document.on('keypress', function(e){
+    if(e.which == 13){
+        $($surface).toggleClass('movingtotheright')
+    }
+})
+
+
+ });
